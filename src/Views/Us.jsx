830e628b1d715dc6b92cbfef13a9ac61383e47ff
Moises-HP <91764978+Moises-HP@users.noreply.github.com>
2024-05-19 23:49:05 -0600

    FixUs

--- src/Views/Us.jsx
+++ src/Views/Us.jsx
@@ -8,13 +8,13 @@ export const Us = () => {
     <>
         <div className="w-full h-[400px] lg:h-[600px] bg-gradient-to-b from-amber-500 to-orange-400 grid grid-cols-12 gap-4 ">
           <div className="mx-8 col-span-12 md:col-span-6 text-center content-center">
-              <span className="font-bold text-[28px] lg:text-[32px] xl:text-[40px] text-white animate-fade-left animation-once animation-duration-2000 animation-ease-linear">Acerca de Nosotros</span>
+              <span className="font-bold text-[28px] lg:text-[32px] xl:text-[40px] text-white animate-fade-left animation-once animation-duration-1000 animation-ease-linear animate-delay-500">Acerca de Nosotros</span>
               <br/><br/>
-              <span className="font-bold text-[22px] lg:text-[26px] xl:text-[34px] text-white animate-fade-left animation-once animation-duration-2000 animation-ease-linear">MoyTect surgió para optimizar el servicio y el seguimiento de las necesidades tecnológicas de nuestros clientes, además de lograr su confianza y satisfacción.</span>
+              <span className="font-bold text-[22px] lg:text-[26px] xl:text-[34px] text-white animate-fade-left animation-once animation-duration-1000 animation-ease-linear animate-delay-500">MoyTect surgió para optimizar el servicio y el seguimiento de las necesidades tecnológicas de nuestros clientes, además de lograr su confianza y satisfacción.</span>
           </div>
-          <div className="col-span-12 hidden md:grid md:col-span-6 text-center content-center justify-self-center">
+          <div className="col-span-12 hidden md:grid md:col-span-6 text-center content-center justify-self-center animate-fade-left animation-once animation-duration-1000 animate-delay-1000 animation-ease-linear">
             <Image
-                className='w-auto h-[350px] lg:h-[400px] xl:h-[550px] animate-fade-left animation-once animation-duration-1000 animation-ease-linear'
+                className='w-auto h-[350px] lg:h-[400px] xl:h-[550px]'
                 alt="Precarga de imagen"
                 src={tecnologia}
             />
